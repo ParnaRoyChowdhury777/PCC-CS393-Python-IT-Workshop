2d9e7b3a15427ada7b2b73c3820ba8118a828f34
Parna Roy Chowdhury <123104247+ParnaRoyChowdhury777@users.noreply.github.com>
2023-12-15 17:25:54 +0530

    Fix formatting and spacing in code

--- Basics/sum.py
+++ Basics/sum.py
@@ -3,4 +3,4 @@
 num1 = int(input("Enter the first number : "))
 num2 = int(input("Enter the second number : "))
 sum = num1 + num2
-print("The Sum of",num1,"and",num2,"is",sum)
+print("The Sum of", num1, "and", num2, "is", sum)
