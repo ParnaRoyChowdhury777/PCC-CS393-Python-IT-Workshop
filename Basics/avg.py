d49dcbcf2cf65fc7be62ed9fb9a6769fa55ea8f9
Parna Roy Chowdhury <123104247+ParnaRoyChowdhury777@users.noreply.github.com>
2023-12-15 17:22:05 +0530

    Fix formatting in avg.py

--- Basics/avg.py
+++ Basics/avg.py
@@ -2,6 +2,6 @@
 
 num1 = int(input("Enter the first number : "))
 num2 = int(input("Enter the second number : "))
-avg=(num1+num2)/2
+avg = (num1+num2)/2
 print("The average of",num1,"and",num2,"is",avg)
 
