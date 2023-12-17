42b01973b87eb9ecfe2038d39082165228997af7
Parna Roy Chowdhury <123104247+ParnaRoyChowdhury777@users.noreply.github.com>
2023-12-18 02:15:04 +0530

    Add exception handling to code

--- Exception Handling/Codes/exception2.py
+++ Exception Handling/Codes/exception2.py
@@ -1,3 +1,13 @@
+# The code below assigns the 5th letter of each word in 'food' to the new list fifth. However, the code currently produces errors. Insert a try/except clause that will allow the code to run and produce a list of the 5th letter in each word. If the word is not long enough, it should not print anything out. Note: The 'pass' statement is a null operation; nothing will happen when it executes.
+
+# food = ["chocolate", "chicken", "corn", "sandwich", "soup", "potatoes", "beef", "lox", "lemonade"]
+
+# fifth = []
+
+# for x in food:
+
+#     fifth.append(x[4])
+
 food = ["chocolate", "chicken", "corn", "sandwich", "soup", "potatoes", "beef", "lox", "lemonade"]
 
 fifth = []
--- Exception Handling/Codes/exception3.py
+++ Exception Handling/Codes/exception3.py
@@ -1,3 +1,13 @@
+# In Python, write an interactive calculator. Take input as command line arguments, which is assumed to be a formula that consist of a number, an operator (at least + and -), and another number, separated by white space (e.g. 1 + 1). Check whether the input arguments are valid before computing the result:
+
+# If the input does not consist of 3 elements, raise a FormulaError, which is a custom Exception.
+
+# Try to convert the first and third input to a float (like so: float_value = float(str_value)). Catch any ValueError that occurs, and instead raise a FormulaError
+
+# If the second input is not '+' or '-', again raise a FormulaError
+
+# If the input is valid, perform the calculation and print the result, else  print appropriate error messages and quit.
+
 class FormulaError(Exception): pass
 
 def length_check(lst) :
